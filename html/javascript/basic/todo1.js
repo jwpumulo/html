b18594f437bd28aebc3bdd8ec9c629935caf1366
Jw Pumulo <j.w.pumulo@gmail.com>
2025-01-15 09:00:05 +0900

    0115

---
+++ html/javascript/basic/todo1.js
@@ -0,0 +1,61 @@
+// //todo1.js
+
+// 변수 
+// 연산자
+// 조건문
+
+
+
+// 1~10 무작위 숫자 3개 생성
+// 3개의 숫자 중 같은 숫자가 몇 개 있는지 console에 출력
+// 예) 생성된 숫자가 7, 7, 4일 경우 2 출력
+// 모두 다를 경우 0 출력
+
+
+
+
+// 두 수 1~100까지 임의수 생성
+// 두 수를 더한 값이 
+// let sumEven = 10; 짝수의 값은 sum Even에 합하기
+// let sumOdd = 20; 홀수의 값은 sum Odd에 합하기
+
+
+
+
+
+
+// ----------------------------------------------
+// 두 수를 입력 받아서 
+// 첫번째 값에 15를 더하고
+// 두번째 값에 2를 나눠서
+// 두 수의 차를 구해보세요
+
+
+
+
+
+// 세 수 중에서 제일 작은 값을 출력
+
+let num1 = parseInt(Math.random() * 100) + 1;
+let num2 = parseInt(Math.random() * 100) + 1;
+let num3 = parseInt(Math.random() * 100) + 1;
+
+console.log(num1, num2, num3);
+
+if (num1 < num2) { if(num1<num3) {result=num1;} else { result=num3;}}
+else {if (num2 < num3) {result=num2;} else {result = num3;}}
+console.log (`가장 작은 수는 ${result}입니다`);
+
+
+// 94576분은 몇 일, 몇 시간, 몇 분 입니까
+> 94576분은 65일 16시간 16분 입니다
+
+
+
+let firstValue = prompt("분을 입력: ");
+let day = parseInt(firstValue / (24*60));
+let hour = parseInt((firstValue % (24*60)) /60);
+let min = firstValue % 60
+;
+
+console.log ( `${firstValue}분은 ${day}일 ${hour}시간 ${min}분 입니다`);
